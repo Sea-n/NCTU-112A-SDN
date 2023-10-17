3e342d2ab77d8fc3791623809a19c266d3329a05
Sean Wei <me@sean.taipei>
2023-10-17 21:13:40 +0800

    Lab 2: add demo

---
+++ Lab2-OpenFlow/project2_312551015/part4/topo_312551015.py
@@ -0,0 +1,16 @@
+from mininet.topo import Topo
+
+class Project2_Topo_312551015(Topo):
+    def __init__(self):
+        Topo.__init__(self)
+
+        s1 = self.addSwitch('s1')
+        h1 = self.addHost('h1', ip='192.168.130.1/27', mac='00:00:00:00:00:01')
+        h2 = self.addHost('h1', ip='192.168.130.2/27', mac='00:00:00:00:00:02')
+        h3 = self.addHost('h1', ip='192.168.130.3/27', mac='00:00:00:00:00:03')
+
+        self.addLink(s1, h1)
+        self.addLink(s1, h2)
+        self.addLink(s1, h3)
+
+topos = {'topo_312551015': Project2_Topo_312551015}
